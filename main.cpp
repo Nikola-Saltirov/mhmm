2f083c47139a1199089101034322cbedf6ccd8de
Никола Салтиров <116568884+Nikola-Saltirov@users.noreply.github.com>
2023-06-14 10:14:55 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,40 @@
 #include <iostream>
 #include <cstring>
 using namespace std;
-class somethings{
+class BasketballPlayer{
 private:
-    char *name;
-    
+    char ime[20],prezime[20];
+    int dres,p1,p2,p3;
+public:
+    BasketballPlayer(){
+    }
+    BasketballPlayer(char Ime[20],char Prezime[20],int Dres, int P1, int P2, int P3){
+        strcpy(ime,Ime);
+        strcpy(prezime,Prezime);
+        dres=Dres;
+        p1=P1;
+        p2=P2;
+        p3=P3;
+    }
+    ~BasketballPlayer(){
+    }
+    float avg(){
+        float average;
+        average=p1+p2+p3;
+        return average/3;
+    }
+    void print(){
+        cout<<"Player: "<<ime<<" "<<prezime<<" with number: ";
+        cout<<dres<<" has "<<avg()<<" point on average"<<endl;
+    };
+
 };
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    BasketballPlayer player;
+    int dres,p1,p2,p3;
+    char ime[20],prezime[20];
+    cin>>ime>>prezime>>dres>>p1>>p2>>p3;
+    player=BasketballPlayer(ime,prezime,dres,p1,p2,p3);
+    player.print();
     return 0;
 }
